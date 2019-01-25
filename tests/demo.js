6f081fbff4d74cc886108862e90a91fb6271349f
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2019-01-25 16:33:55 +0100

    Update demo.js

--- tests/demo.js
+++ tests/demo.js
@@ -1,35 +1,82 @@
 module.exports = {
-  '@disabled': false,
-
-  'demo test google' : function (client) {
-    
-    client
-      .url('http://google.com')
-      .waitForElementPresent('body', 1000);
-  },
-
-  'part two' : function(client) {
-    client
-      .setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
+  'Google Search' : function (browser) {
+    browser
+      .url('https://www.google.com')
+      .waitForElementVisible('input[name="q"]', 4000)
+      .setValue('input[name="q"]', 'hello world')
+      .keys(browser.Keys.ENTER)
+      .waitForElementVisible('#resultStats', 4000)
+      .assert.urlContains('q=hello+world')
       .end();
   },
-  
-  'fail demo': function (client) {
-    client 
-      .url('https://bastiangeneugelijk.com')
-      .waitForElementPresent('body', 1000)
-  }, 
-  
-  'fail demo part two': function (client) {
-    client
-      .setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
+
+  'Google Search with Fruits' : function (browser) {
+    browser
+      .url('https://www.google.com')
+      .waitForElementVisible('input[name="q"]', 4000)
+      .setValue('input[name="q"]', 'Fruits')
+      //.keys(browser.Keys.ENTER)
+      .waitForElementVisible('#resultStats', 4000)
+      .assert.urlContains('q=Fruits')
+      .end();
+  },
+
+  'Google Search with Food' : function (browser) {
+    browser
+      .url('https://www.google.com')
+      .waitForElementVisible('input[name="q"]', 4000)
+      .setValue('input[name="q"]', 'Food')
+      .keys(browser.Keys.ENTER)
+      .waitForElementVisible('#resultStats', 4000)
+      .assert.urlContains('q=Food')
+      .end();
+  },
+
+  'Google Search with House' : function (browser) {
+    browser
+      .url('https://www.google.com')
+      .waitForElementVisible('input[name="q"]', 4000)
+      .setValue('input[name="q"]', 'House')
+      .keys(browser.Keys.ENTER)
+      .waitForElementVisible('#resultStats', 4000)
+      .assert.urlContains('q=House')
       .end();
   }
-};
+
+  };
+
+// module.exports = {
+//   '@disabled': false,
+
+//   'demo test google' : function (client) {
+    
+//     client
+//       .url('http://google.com')
+//       .waitForElementPresent('body', 1000);
+//   },
+
+//   'part two' : function(client) {
+//     client
+//       .setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
+//       .pause(1000)
+//       .assert.containsText('#main', 'Night Watch')
+//       .end();
+//   },
+  
+//   'fail demo': function (client) {
+//     client 
+//       .url('https://bastiangeneugelijk.com')
+//       .waitForElementPresent('body', 1000)
+//   }, 
+  
+//   'fail demo part two': function (client) {
+//     client
+//       .setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
+//       .pause(1000)
+//       .assert.containsText('#main', 'Night Watch')
+//       .end();
+//   }
+// };
 
 // module.exports = {
 //   'Demo test': browser => {
