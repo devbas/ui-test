e07a498eef7edb7ddd9f1302b5933396eeb1f3b3
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2019-01-25 11:06:17 +0100

    Update demo.js

--- tests/demo.js
+++ tests/demo.js
@@ -1,5 +1,8 @@
 module.exports = {
   'Demo test' : function (browser) {
+    browser.session(function(session) {
+      console.log(session.sessionId);
+    });
     browser
       .url(browser.launchUrl)
       // ...
