ad8de1eb998596b8dc74d630b5c65729a2c9d757
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2019-01-25 12:02:32 +0100

    Update demo.js

--- tests/demo.js
+++ tests/demo.js
@@ -1,9 +1,27 @@
 module.exports = {
-  'Demo test': browser => {
-    
-    browser
-      .url(browser.launchUrl)
-      .end()
-    
+  '@disabled': false,
+
+  'demo test google' : function (client) {
+    client
+      .url('http://google.com')
+      .waitForElementPresent('body', 1000);
+  },
+
+  'part two' : function(client) {
+    client
+      .setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
   }
-}
+};
+
+// module.exports = {
+//   'Demo test': browser => {
+    
+//     browser
+//       .url(browser.launchUrl)
+//       .end()
+    
+//   }
+// }
